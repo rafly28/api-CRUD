3bab8b4b6cc858181830373f1e154d674e35f06e
aptavis <raflyfirdaus28@gmail.com>
2024-08-31 08:47:25 +0700

    setup dotenv and optimize requirements

--- app/database.py
+++ app/database.py
@@ -1,9 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Ganti dengan URL database yang Anda gunakan
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:12345678@127.0.0.1/laravel_devel"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 # Buat engine tanpa `connect_args`
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
